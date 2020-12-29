c99317154d26fc6173017eb8cdebe4d873b709c5
LHoogenboom <hoogenboom.l.p@gmail.com>
2020-12-29 16:15:40 +0100

    adds basic install and update scripts for automation

---
+++ install.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+cp bspwmrc ~/.config/bspwm/bspwmrc
+cp sxhkdrc ~/.config/sxhkd/sxhkdrc
+cp config ~/.config/polybar/config
+
+cp .bashrc ~/.bashrc
+cp .bash_profile  ~/.bash_profile
+
+cp -R .wallpapers ~/.wallpapers/
---
+++ update.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+cp ~/.config/bspwm/bspwmrc .
+cp ~/.config/sxhkd/sxhkdrc .
+cp ~/.config/polybar/config .
+
+cp ~/.bashrc .
+cp ~/.bash_profile .
+
+cp -R  ~/.wallpapers/ .
